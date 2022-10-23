db6f2a769a996e7e0ceada1b42c0fc51c1b28391
Ttiki <clem64.combier@gmail.com>
2022-10-23 10:12:19 +0200

    Initial commit

---
+++ app/src/main/java/com/ttiki/monthlyexpenses/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ttiki.monthlyexpenses
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigatin_view)
+        //create nav controller object
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        //setup bottom navigation view with nav controller
+        bottomNavigationView.setupWithNavController(navController)
+
+
+
+
+    }
+}
